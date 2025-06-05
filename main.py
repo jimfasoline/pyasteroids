01ec3fcbd43e19ff370d763c07cbe2fffbfc730c
jimfasoline <jimfasoline@hotmail.com>
2025-06-05 01:52:33 -0400

    Implemeted player, asteroid field

---
+++ main.py
@@ -0,0 +1,54 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+
+    adteroidfield = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+               return
+        
+        updatable.update(dt)
+            
+        screen.fill('black')
+
+        for object in drawable:
+            object.draw(screen)
+
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
+
+
+if __name__ == "__main__":
+    main()
